5417701b8c469a57c62f336bea6dca163457f6a3
jeanhadrien <40248338+jeanhadrien@users.noreply.github.com>
2023-03-05 14:04:07 +0100

    day2 + comments

--- day1.php
+++ day1.php
@@ -1,36 +1,36 @@
-<a href=/adventofcode2022/> Back to list </a> </br> --- </br></br>
+<a href=/adventofcode2022 /> Back to list </a> </br> --- </br></br>
 
 <?php
 
-// Day 1 
 // Part 1 : Find the elf with most calories, how many calories are they carrying ?
-// Part 2 : Find the 3 top elf with most calories, how many calories are they carrying in total?
 
 $sum_current_elf_cal = 0;
-
+$elf_calories = [];
 $lines = file("./input/day1-calories.txt");
 
-$arr_calories = [];
-
 foreach ($lines as $line) {
+    // If empty line, save total for current elf and reset
     if ($line == "\n" or $line == "\r") {
-        $arr_calories[] = $sum_current_elf_cal;
+        $elf_calories[] = $sum_current_elf_cal;
         $sum_current_elf_cal = 0;
     }
+
+    // Keep track of the current elf total calories
     $cal = intval($line);
     $sum_current_elf_cal += intval($cal);
 }
 
-rsort($arr_calories);
+rsort($elf_calories);
+echo ("Elf calories in descending order : " . implode(", ", $elf_calories));
+echo ("<br><br> Calories carried by the top elf : " . current($elf_calories));
 
-echo ("Elf calories in descending order : " . implode(", ", $arr_calories));
-
-echo ("<br><br> Calories carried by the top elf : " . current($arr_calories));
+// Part 2 : Find the 3 top elf with most calories, how many calories are they carrying in total?
 
 $top3sum = 0;
+
 for ($i = 1; $i <= 3; $i++) {
-    $top3sum += current($arr_calories);
-    next($arr_calories);
+    $top3sum += current($elf_calories);
+    next($elf_calories);
 }
 
 echo ("<br><br> Calories carried by the top 3 elves : " . $top3sum);
--- day2.php
+++ day2.php
@@ -1,6 +1,113 @@
-<a href=/adventofcode2022/> Back to list </a> </br> --- </br></br>
+<a href=/adventofcode2022 /> Back to list </a> </br> --- </br></br>
 
 <?php
 
+$wins = array("X" => "C", "Y" => "A", "Z" => "B");
+
+function isWin($me, $other)
+{
+    global $wins;
+    if ($wins[$me] == $other)
+        return true;
+    return false;
+}
+
+$draws = array("X" => "A","Y" => "B","Z" => "C");
+
+function isDraw($me, $other)
+{
+    global $draws;
+    if ($draws[$me] == $other) {
+        return true;
+    }
+    return false;
+}
+
+function getShapeScore($me)
+{
+    if ($me == "X") {
+        return 1;
+    } elseif ($me == "Y") {
+        return 2;
+    }
+    return 3;
+}
+
+function getOutcomeScore($me, $other)
+{
+    if (isWin($me, $other))
+        return 6;
+    if (isDraw($me, $other))
+        return 3;
+    return 0;
+}
+
+function getTotalScore($me, $other)
+{
+    return getShapeScore($me) + getOutcomeScore($me, $other);
+}
+
+// Part 1 : Calculate the total score when XYZ are the moves I play during the round.
+
+$lines = file("./input/day2-input.txt");
+
+$total = 0;
+foreach ($lines as $line) {
+
+    // Get round moves for each player
+    $line = preg_replace("/[^A-Za-z]/", '', $line);
+    $moves = str_split($line);
+    $other = current($moves);
+    next($moves);
+    $me = current($moves);
+
+    // Calculate round score based on played moves
+    $roundScore = getTotalScore($me, $other);
+
+    // Keep track of the total
+    $total += $roundScore;
+}
+
+echo "Total score Part 1 : " . $total;
+echo "<br><br>";
+
+// Part 2 : Calculate the total score when XYZ are desired outcomes of the rounds.
+
+$losses = array(
+    "X" => "B",
+    "Y" => "C",
+    "Z" => "A",
+);
+
+function getMoveToPlay($goal, $other)
+{
+    global $wins, $losses, $draws;
+    if ($goal == "X") // want to lose
+        return array_search($other, $losses); 
+    if ($goal == "Y") // want to draw
+        return array_search($other, $draws); 
+    if ($goal == "Z") // want to win
+        return array_search($other, $wins); 
+}
+
+$total = 0;
+foreach ($lines as $line) {
+
+    // Get round moves for each player
+    $line = preg_replace("/[^A-Za-z]/", '', $line);
+    $moves = str_split($line);
+    $other = current($moves);
+    next($moves);
+    $goal = current($moves);
+    $me = getMoveToPlay($goal, $other);
+
+    // Calculate round score based on played moves
+    $roundScore = getTotalScore($me, $other);
+
+    // Keep track of the total
+    $total += $roundScore;
+}
+
+echo "Total score Part 2 : " . $total;
 
 ?>
